1d68fb21fc8d2044d1ebe8ae5034c6d95a86490c
matveevartem <www.artem.matveev@gmail.com>
2023-09-17 09:52:41 +0300

    Update Counter.php
    Перепутал (или опечатка) позиции '' и ' ' в Counter (а надо наоборот). Исправил.

--- src/Tasks/Counting/Counter.php
+++ src/Tasks/Counting/Counter.php
@@ -30,7 +30,7 @@ class Counter
     protected function calculateFromFile(\DirectoryIterator $iterator)
     {
         $content = file_get_contents($iterator->getPathname());
-        $content = preg_replace('/[ ]+/', ' ', preg_replace('/[^0-9 \.]/', '', str_replace(PHP_EOL, ' ' ,$content)));
+        $content = preg_replace('/[ ]+/', ' ', preg_replace('/[^0-9 \.]/', ' ', str_replace(PHP_EOL, '' , $content)));
         foreach (explode(' ', $content) as $value) {
             $this->count += floatval($value);
         }
